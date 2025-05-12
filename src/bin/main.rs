4e233098ba3fe2605427637989534f27f68722df
tietang <30461739@qq.com>
2025-05-12 12:00:17 +0800

    migration to rp2040-hal 0.11.0

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -15,6 +15,10 @@ use rp2040_hal::{
     watchdog::Watchdog,
 };
 
+#[link_section = ".boot2"]
+#[used]
+pub static BOOT_LOADER: [u8; 256] = rp2040_boot2::BOOT_LOADER_W25Q080;
+
 #[entry]
 fn main() -> ! {
     info!("Program start");
